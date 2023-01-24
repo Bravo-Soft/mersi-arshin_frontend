c06190a47fb48e1e68dd673aeaefb6e363972c69
fkingBug <fkingbug@gmail.com>
2023-01-24 11:24:43 +0300

    fix_func

--- src/components/Forms/hooks/useAutocomplete.ts
+++ src/components/Forms/hooks/useAutocomplete.ts
@@ -1,63 +1,27 @@
 import { useGetAllDataQuery } from 'features/dataTable/dataTableApiSlice';
 import { validateAutcompleteParams } from '../utils/validateAutocomplete';
-import { getArrayWithoutDuplicates } from 'utils/getArrayWithoutDuplicates';
 
 import type { IDataItem } from 'types/dataItem';
 
-type AutocompleteKeysType = 'name' | 'type' | 'division' | 'condition' | 'accuracyClass' | 'notes';
+export type AutocompleteKeysType = keyof Omit<
+	IDataItem,
+	| 'id'
+	| 'size'
+	| 'userIds'
+	| 'documents'
+	| 'verificationDate'
+	| 'dateOfTheNextVerification'
+	| 'productionDate'
+>;
 
-export type AutocompleteType = Record<AutocompleteKeysType, IDataItem[AutocompleteKeysType][]>;
-
-const defaultValue = {
-	name: [],
-	type: [],
-	notes: [],
-	division: [],
-	condition: [],
-	accuracyClass: [],
-};
-
-export const useAutocomplete = (keysArray: string[]) => {
+export const useAutocomplete = (keysArray: AutocompleteKeysType[]) => {
 	const { data: loadedData = [] } = useGetAllDataQuery();
 
-	const createdArrayAutocompleteFirst = loadedData.reduce((acc, cc) => {}, {});
-
-	// const createdArrayAutocompleteFirst = loadedData
-	// 	.map(e =>
-	// 		Object.entries(e).filter(([key, value]) => {
-	// 			return keysArray.includes(key);
-	// 		})
-	// 	)
-	// 	.reduce((obj, [keys, values]) => {
-	// 		return { ...obj, [keys]: values };
-	// 	}, {});
-
-	return createdArrayAutocompleteFirst;
+	return validateAutcompleteParams(
+		keysArray
+			.map(key => loadedData.map(item => item[key]))
+			.reduce((acc, el, i) => {
+				return { ...acc, [keysArray[i]]: el };
+			}, {}) as Record<AutocompleteKeysType, string[]>
+	);
 };
-// const createdArrayAutocompleteFirst = Object.entries(loadedData).filter(([key, value]) => {
-// 	console.log('keysArray', keysArray);
-// 	console.log('value', value);
-// 	return keysArray.includes(key);
-// });
-// .reduce((obj, [keys, value]) => {
-// 	return { ...obj, keys: value };
-// }, {});
-
-// return createdArrayAutocompleteFirst;
-
-// const createdArrayAutocomplete = loadedData.reduce<AutocompleteType>(
-// 	(acc, { name, type, division, condition, accuracyClass, notes }): AutocompleteType => {
-// 		return {
-// 			name: validateAutcompleteParams(acc.name, name),
-// 			type: validateAutcompleteParams(acc.type, type),
-// 			notes: validateAutcompleteParams(acc.notes, notes),
-// 			division: validateAutcompleteParams(acc.division, division),
-// 			condition: validateAutcompleteParams(acc.condition, condition),
-// 			accuracyClass: validateAutcompleteParams(acc.accuracyClass, accuracyClass),
-// 		};
-// 	},
-// 	defaultValue
-// );
-// return Object.entries(createdArrayAutocomplete).reduce((acc, [key, value]) => {
-// 	return { ...acc, [key]: getArrayWithoutDuplicates(...value) };
-// }, {});
--- src/components/Forms/utils/validateAutocomplete.ts
+++ src/components/Forms/utils/validateAutocomplete.ts
@@ -1,6 +1,21 @@
 import { isValueDefined } from 'guards/isValueDefined';
+import { getArrayWithoutDuplicates } from 'utils/getArrayWithoutDuplicates';
 
-export const validateAutcompleteParams = (array: string[], str: string) =>
-	isValueDefined(str) ? [...array, deleteSpaces(str)] : array;
+import type { AutocompleteKeysType } from '../hooks/useAutocomplete';
+
+export const validateAutcompleteParams = (obj: Record<AutocompleteKeysType, string[]>) => {
+	return Object.fromEntries(
+		Object.entries(obj).map(([key, value]) => [key, validateAutcompleteParams2(value)])
+	);
+};
+
+export const validateAutcompleteParams2 = (array: string[]) => {
+	return getArrayWithoutDuplicates(
+		...array
+			.filter(e => isValueDefined(e))
+			.filter(e => e)
+			.map(str => deleteSpaces(str))
+	);
+};
 
 export const deleteSpaces = (str: string) => str.replace(/\s+/g, ' ').trim();
