d9f85d091712888117f17c17c15b6ec215c827d1
TheRebellionDreamer <rebellion.dreamer@yandex.ru>
2023-01-30 14:53:24 +0300

    FIX: Change type of args

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,10 +1,10 @@
 import { useCallback, useRef } from 'react';
 
-export const useDebounce = <T, Y>(callback: (...args: T[]) => Y, delay: number) => {
+export const useDebounce = <T>(callback: (...args: any[]) => T, delay: number) => {
 	const timer = useRef<NodeJS.Timeout>();
 
 	const debouncedCallback = useCallback(
-		(...args: T[]) => {
+		(...args: any[]) => {
 			if (timer.current) {
 				clearTimeout(timer.current);
 			}
