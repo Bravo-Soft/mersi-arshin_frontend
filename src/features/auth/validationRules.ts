e813a73d0b8c309bc5245909d68787b339b760c8
TheRebellionDreamer <rebellion.dreamer@yandex.ru>
2023-01-30 15:53:29 +0300

    UPDATE: Added connection of validation rules depending on the environment variable

--- src/features/auth/validationRules.ts
+++ src/features/auth/validationRules.ts
@@ -1,4 +1,5 @@
 import type { RegisterOptions } from 'react-hook-form';
+import { getEnvValue } from 'utils/getEnvValue';
 import type { IAuthFormRequest } from './authApiSlice';
 
 type AuthValidationRules = Record<keyof IAuthFormRequest, RegisterOptions>;
@@ -7,8 +8,19 @@ export const minLength = 8;
 export const maxLength = 15;
 
 export const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/gi;
-// const passwordPattern = // TODO: Вернуть правила валидации пароля
-// 	/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[,.;:!?\-_()])[a-zA-Z0-9,.;:!?\-_()]+$/g;
+const passwordPattern =
+	/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[,.;:!?\-_()])[a-zA-Z0-9,.;:!?\-_()]+$/g;
+
+const getPasswordRules = (): Pick<RegisterOptions, 'maxLength' | 'pattern'> => ({
+	maxLength: {
+		value: maxLength,
+		message: `Максимальная длина пароля ${maxLength} символов`,
+	},
+	pattern: {
+		value: passwordPattern,
+		message: 'Пароль не соответствует правилам безопасности',
+	},
+});
 
 export const validationRules: AuthValidationRules = {
 	email: {
@@ -24,13 +36,6 @@ export const validationRules: AuthValidationRules = {
 			value: minLength,
 			message: `Минимальная длина пароля ${maxLength} символов`,
 		},
-		// maxLength: {
-		// 	value: maxLength,
-		// 	message: `Максимальная длина пароля ${maxLength} символов`,
-		// },
-		// pattern: {
-		// 	value: passwordPattern,
-		// 	message: 'Пароль не соответствует правилам безопасности',
-		// },
+		...(JSON.parse(getEnvValue('ENABLE_PASSWORD_RULES')) && getPasswordRules()),
 	},
 };
