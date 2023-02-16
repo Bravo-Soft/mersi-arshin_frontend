a538567f1e8f2581202b21add6286e56a6522022
Fking Bug <fkingbug@gmail.com>
2023-02-16 07:04:17 +0300

    2_type_menu_item

--- src/features/dataTable/modules/Printing/components/TagContextMenu.tsx
+++ src/features/dataTable/modules/Printing/components/TagContextMenu.tsx
@@ -12,7 +12,7 @@ import TextField from '@mui/material/TextField';
 import ListItemText from '@mui/material/ListItemText';
 import SearchOffIcon from '@mui/icons-material/SearchOff';
 import Typography from '@mui/material/Typography';
-import { FormControlLabel, ListItem } from '@mui/material';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 interface ITagContextMenuProps {
 	activeTag: ISortedTagItems;
@@ -59,10 +59,10 @@ function TagContextMenu({
 			}
 			PaperProps={{ sx: { display: 'flex', height: 310, width: 240, ...hideScrollbar() } }}
 			MenuListProps={{
-				sx: { display: 'flex', flexDirection: 'column', flexGrow: 1 },
+				sx: { display: 'flex', flexDirection: 'column', flexGrow: 1, p: 0 },
 			}}
 		>
-			<Box px={1.5}>
+			<MenuItem dense>
 				<TextField
 					fullWidth
 					placeholder='Значение фильтра'
@@ -71,12 +71,13 @@ function TagContextMenu({
 					value={fieldValue}
 					onKeyDown={handleOnKeyDown}
 				/>
-			</Box>
+			</MenuItem>
 
 			{renderPrintingValue.length ? (
 				renderPrintingValue.map(e => (
-					<MenuItem>
+					<MenuItem key={e.translatedKey} dense>
 						<FormControlLabel
+							sx={{ m: 0, width: '100%' }}
 							control={
 								<Switch
 									edge='start'
@@ -90,7 +91,7 @@ function TagContextMenu({
 									}}
 								/>
 							}
-							label={e.translatedKey}
+							label={<ListItemText id={e.translatedKey} primary={e.translatedKey} />}
 						/>
 					</MenuItem>
 				))
@@ -110,3 +111,115 @@ function TagContextMenu({
 }
 
 export default TagContextMenu;
+
+// import { useState } from 'react';
+// import { hideScrollbar } from 'utils/hideScrollbar';
+
+// import type { ISortedTagItems } from '../utils/printingSort';
+// import type { ICoordinates } from 'features/dataTable/hooks/useContextMenuActions';
+
+// import Box from '@mui/material/Box';
+// import Menu from '@mui/material/Menu';
+// import MenuItem from '@mui/material/MenuItem';
+// import Switch from '@mui/material/Switch';
+// import TextField from '@mui/material/TextField';
+// import ListItemText from '@mui/material/ListItemText';
+// import SearchOffIcon from '@mui/icons-material/SearchOff';
+// import Typography from '@mui/material/Typography';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+
+// interface ITagContextMenuProps {
+// 	activeTag: ISortedTagItems;
+// 	coordinates: ICoordinates | null;
+// 	handleClose: () => void;
+// 	handleClickPrintingContextMenu: (idx: string, name: string) => void;
+// }
+
+// function TagContextMenu({
+// 	activeTag,
+// 	handleClose,
+// 	handleClickPrintingContextMenu,
+// 	coordinates,
+// }: ITagContextMenuProps) {
+// 	const [renderPrintingValue, setRenderPrintingValue] = useState(activeTag.tagsPrint);
+// 	const [fieldValue, setFieldValue] = useState('');
+
+// 	const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+// 		event.preventDefault();
+// 		setRenderPrintingValue(
+// 			activeTag.tagsPrint.filter(e =>
+// 				e.translatedKey.toLowerCase().trim().includes(event.target.value.toLowerCase().trim())
+// 			)
+// 		);
+// 		setFieldValue(event.target.value);
+// 	};
+
+// 	const onClickMenuContext =
+// 		(idx: string, name: string) => (event: React.MouseEvent<HTMLElement>) => {
+// 			handleClickPrintingContextMenu(idx, name);
+// 			setRenderPrintingValue(prev =>
+// 				prev.map(e => (e.translatedKey === name ? { ...e, isVisible: !e.isVisible } : e))
+// 			);
+// 		};
+// 	const handleOnKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => () => e.stopPropagation();
+
+// 	return (
+// 		<Menu
+// 			open={Boolean(coordinates)}
+// 			onClose={handleClose}
+// 			anchorReference='anchorPosition'
+// 			anchorPosition={
+// 				coordinates ? { top: coordinates.mouseY, left: coordinates.mouseX } : undefined
+// 			}
+// 			PaperProps={{ sx: { display: 'flex', height: 310, width: 240, ...hideScrollbar() } }}
+// 			MenuListProps={{
+// 				sx: { display: 'flex', flexDirection: 'column', flexGrow: 1 },
+// 			}}
+// 		>
+// 			<MenuItem dense>
+// 				<TextField
+// 					fullWidth
+// 					placeholder='Значение фильтра'
+// 					onChange={handleSearch}
+// 					size='small'
+// 					value={fieldValue}
+// 					onKeyDown={handleOnKeyDown}
+// 				/>
+// 			</MenuItem>
+
+// 			{renderPrintingValue.length ? (
+// 				renderPrintingValue.map(e => (
+// 					<MenuItem
+// 						key={e.translatedKey}
+// 						onClick={onClickMenuContext(activeTag.id, e.translatedKey)}
+// 						dense
+// 					>
+// 						<Switch
+// 							edge='start'
+// 							size='small'
+// 							checked={e.isVisible}
+// 							tabIndex={-1}
+// 							disableRipple
+// 							inputProps={{
+// 								'aria-labelledby': 'switch-list-label-wifi',
+// 							}}
+// 						/>
+// 						<ListItemText id={e.translatedKey} primary={e.translatedKey} />
+// 					</MenuItem>
+// 				))
+// 			) : (
+// 				<Box
+// 					display='flex'
+// 					flexDirection='column'
+// 					alignItems='center'
+// 					justifyContent='center'
+// 					flexGrow={1}
+// 				>
+// 					<SearchOffIcon /> <Typography component='span'>Значение не найдено</Typography>
+// 				</Box>
+// 			)}
+// 		</Menu>
+// 	);
+// }
+
+// export default TagContextMenu;
