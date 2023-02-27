374b91d72da41ee01aa51009df146b2c21207e37
TheRebellionDreamer <rebellion.dreamer@yandex.ru>
2023-02-27 11:51:31 +0300

    Added end year

--- src/components/Layout/LayoutFooter.tsx
+++ src/components/Layout/LayoutFooter.tsx
@@ -1,11 +1,13 @@
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
+import { getYear } from 'date-fns';
+
 function LayoutFooter(): JSX.Element {
 	return (
 		<Toolbar variant='dense' sx={{ justifyContent: 'center' }}>
 			<Typography variant='caption' color='text.secondary'>
-				© ООО «Браво Софт», 2022
+				© ООО «Браво Софт», 2022 – {getYear(new Date())}
 			</Typography>
 		</Toolbar>
 	);
