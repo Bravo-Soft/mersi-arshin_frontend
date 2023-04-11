527007bd29ae581b829274e3684c918d8c61c174
fkingBug <fkingbug@gmail.com>
2023-04-11 10:12:48 +0300

    add_max_size

--- src/utils/getExtendedIntervalRules.ts
+++ src/utils/getExtendedIntervalRules.ts
@@ -1,8 +1,12 @@
 import type { RegisterOptions } from 'react-hook-form';
 
-export const getExtendedIntervalRules = (): Pick<RegisterOptions, 'min'> => ({
+export const getExtendedIntervalRules = (): Pick<RegisterOptions, 'min' | 'max'> => ({
 	min: {
 		value: 1,
 		message: `Интервал поверки не может быть меньше или равным 0`,
+	},
+	max: {
+		value: 1000,
+		message: 'Интервал поверки не может быть больше 100',
 	},
 });
