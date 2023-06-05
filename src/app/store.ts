2519c52891ad9e8023752c51ac05ac5527891cc5
TheRebellionDreamer <rebellion.dreamer@yandex.ru>
2023-06-05 16:22:11 +0300

    Remove redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,3 @@
-import storage from 'redux-persist/lib/storage';
-
-import type { AnyAction, Reducer } from '@reduxjs/toolkit';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import { authPath, authReducer } from 'features/auth/authSlice';
@@ -9,29 +6,11 @@ import { notificationPath, notificationReducer } from 'features/notificator/noti
 import { sidebarPath, sidebarReducer } from 'features/sidebar/sidebarSlice';
 import { smartDialogPath, smartDialogReducer } from 'features/smartDialog/smartDialogSlice';
 import { userPath, userReducer } from 'features/user/userSlice';
-import {
-	FLUSH,
-	PAUSE,
-	PERSIST,
-	persistReducer,
-	persistStore,
-	PURGE,
-	REGISTER,
-	REHYDRATE,
-} from 'redux-persist';
 import { apiSlice } from './apiSlice';
 
-import type { PersistConfig } from 'redux-persist';
 import { quickTourPath, quickTourReducer } from 'features/quickTour/components/quickTourSlice';
 
-const config: PersistConfig<RootState> = {
-	key: 'root',
-	version: 1,
-	storage,
-	whitelist: [authPath],
-};
-
-const combineReducer = combineReducers({
+const rootReducer = combineReducers({
 	[apiSlice.reducerPath]: apiSlice.reducer,
 	[authPath]: authReducer,
 	[notificationPath]: notificationReducer,
@@ -42,30 +21,13 @@ const combineReducer = combineReducers({
 	[quickTourPath]: quickTourReducer,
 });
 
-const rootReducer: Reducer = (state: RootState, action: AnyAction) => {
-	if (action.type === 'auth/resetCredentionals') {
-		state = {} as RootState;
-	}
-
-	return combineReducer(state, action);
-};
-
-const persistedReducer = persistReducer(config, rootReducer);
-
 export const store = configureStore({
-	reducer: persistedReducer,
+	reducer: rootReducer,
 	devTools: process.env.NODE_ENV === 'development',
-	middleware: getDefaultMiddleware =>
-		getDefaultMiddleware({
-			serializableCheck: {
-				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-			},
-		}).concat(apiSlice.middleware),
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 setupListeners(store.dispatch);
 
-export const persistor = persistStore(store);
-
-export type RootState = ReturnType<typeof combineReducer>;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,37 @@
 import App from 'App';
 import ReactDOM from 'react-dom/client';
+import ruLocale from 'date-fns/locale/ru';
 
-import { theme } from 'theme';
 import { ThemeProvider } from '@mui/material/styles';
-import { LicenseInfo } from '@mui/x-license-pro';
-import { persistor, store } from 'app/store';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
-import { LICENSE_KEY } from 'constant/licenseKey';
-import { register as registerServiceWorker } from './serviceWorkerRegistration';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import ruLocale from 'date-fns/locale/ru';
 import { ruRU } from '@mui/x-date-pickers/locales';
+import { LicenseInfo } from '@mui/x-license-pro';
+import { store } from 'app/store';
+import { LICENSE_KEY } from 'constant/licenseKey';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { theme } from 'theme';
+import { register as registerServiceWorker } from './serviceWorkerRegistration';
+
 LicenseInfo.setLicenseKey(LICENSE_KEY);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
-	<PersistGate loading={null} persistor={persistor}>
-		<Provider store={store}>
-			<ThemeProvider theme={theme}>
-				<LocalizationProvider
-					adapterLocale={ruLocale}
-					localeText={ruRU.components.MuiLocalizationProvider.defaultProps.localeText}
-					dateAdapter={AdapterDateFns}
-				>
-					<BrowserRouter>
-						<App />
-					</BrowserRouter>
-				</LocalizationProvider>
-			</ThemeProvider>
-		</Provider>
-	</PersistGate>
+	<Provider store={store}>
+		<ThemeProvider theme={theme}>
+			<LocalizationProvider
+				adapterLocale={ruLocale}
+				localeText={ruRU.components.MuiLocalizationProvider.defaultProps.localeText}
+				dateAdapter={AdapterDateFns}
+			>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</LocalizationProvider>
+		</ThemeProvider>
+	</Provider>
 );
 
 registerServiceWorker();
