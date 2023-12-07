ba24cba23e73cb2d035cdf30107f57c3e7284b12
Hi, I’m @fkingbug. <fkingbug@gmail.com>
2023-12-07 11:01:15 +0300

    Изменил валидацию email в форме логина

--- src/features/auth/authResolver.ts
+++ src/features/auth/authResolver.ts
@@ -5,7 +5,15 @@ const authSchema = z.object({
 	email: z
 		.string()
 		.transform(e => e.trim())
-		.pipe(z.string().email('Введите email в верном формате').min(1, 'Введите почту')),
+		.pipe(
+			z
+				.string()
+				.regex(
+					new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$'),
+					'Введите email в верном формате'
+				)
+				.min(1, 'Введите почту')
+		),
 	password: z.string().min(8, 'Минимальная длина пароля 8 символов'),
 	// .regex(new RegExp('/[^A-Za-z0-9]/'), 'Пароль не соответствует правилам безопасности'),
 });
