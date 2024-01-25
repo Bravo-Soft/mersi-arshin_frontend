afee25179d5c84fecec3cbaf693e3705eaa92e23
TheRebellionDreamer <rebellion.dreamer@yandex.ru>
2024-01-25 13:05:12 +0300

    feat: create activate func with throwing error, if key is undefined

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,23 @@
 import { ThemeProvider } from "@mui/material/styles";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
-import { LicenseInfo } from "@mui/x-license-pro";
 import dayjs from "dayjs";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import "dayjs/locale/ru";
 
 import { register as registerServiceWorker } from "./serviceWorkerRegistration";
+import { activateMuiX } from './utils/activate-mui-x';
 
 import App from "App";
 import { store } from "app/store";
-import { LICENSE_KEY } from "constant/licenseKey";
 import { theme } from "theme";
 
-import "dayjs/locale/ru";
-
-
-LicenseInfo.setLicenseKey(LICENSE_KEY);
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 dayjs.locale("ru"); // глобальная локализация дат
+activateMuiX()
 
 root.render(
   <Provider store={store}>
