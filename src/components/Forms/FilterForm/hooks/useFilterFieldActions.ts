9db4e2dfe2658af52ca3df6ae000734f3d1111cb
Hi, I’m @fkingbug. <fkingbug@gmail.com>
2024-03-01 14:45:47 +0300

    Рефактор лога

--- src/components/Forms/FilterForm/hooks/useFilterFieldActions.ts
+++ src/components/Forms/FilterForm/hooks/useFilterFieldActions.ts
@@ -3,7 +3,6 @@ import { columnsFilters } from '../../NotificationSettings/data';
 export const useFilterFieldActions = () => {
 	const filterType = (typeFilter: string | boolean) => {
 		const fondedFilter = columnsFilters.find(e => e.field === typeFilter);
-		console.log('fondedFilter', fondedFilter);
 		if (fondedFilter) {
 			return fondedFilter.type;
 		}
