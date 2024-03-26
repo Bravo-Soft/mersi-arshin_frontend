6ccbef1de8df041c431229d5dc7ebc5d2c2a6f7d
fkingbug <fkingbug@gmail.com>
2024-03-26 10:07:03 +0300

    Revert "изменил настойки печати"

--- src/components/Forms/PrintSettings/utils/printSettingsResolver.ts
+++ src/components/Forms/PrintSettings/utils/printSettingsResolver.ts
@@ -1,18 +1,18 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 
-const errorMessage = 'Число должно быть > 0';
+const errorMessage = 'Число должно быть >= 0';
 
 const printSettingsSchema = z.object({
-	'large-font': z.number().positive(errorMessage),
-	'large-height': z.number().positive(errorMessage),
-	'large-width': z.number().positive(errorMessage),
-	'medium-font': z.number().positive(errorMessage),
-	'medium-height': z.number().positive(errorMessage),
-	'medium-width': z.number().positive(errorMessage),
-	'small-font': z.number().positive(errorMessage),
-	'small-height': z.number().positive(errorMessage),
-	'small-width': z.number().positive(errorMessage),
+	'large-font': z.number().nonnegative(errorMessage),
+	'large-height': z.number().nonnegative(errorMessage),
+	'large-width': z.number().nonnegative(errorMessage),
+	'medium-font': z.number().nonnegative(errorMessage),
+	'medium-height': z.number().nonnegative(errorMessage),
+	'medium-width': z.number().nonnegative(errorMessage),
+	'small-font': z.number().nonnegative(errorMessage),
+	'small-height': z.number().nonnegative(errorMessage),
+	'small-width': z.number().nonnegative(errorMessage),
 });
 
 export const printSettingsResolver = zodResolver(printSettingsSchema);
