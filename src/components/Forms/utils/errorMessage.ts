66cccdb5de15d0128601dbefac6729d336d58261
YuriKorepov <korepov@bravosoft.nnov.ru>
2024-06-19 12:46:03 +0300

    Мерси: расширены лимиты столбцов Название - максимально 512 символов Местоположение - максимально 512 символов Доп. данные - максимально 1024 символов Примечание - максимально 1024 символов

--- src/components/Forms/utils/errorMessage.ts
+++ src/components/Forms/utils/errorMessage.ts
@@ -1,4 +1,6 @@
 const errorMessage = (size: number) => `Длина строки не может быть больше ${size}`;
 
+export const biggerLengthField = errorMessage(1024);
+export const mediumLengthField = errorMessage(512);
 export const largeLengthField = errorMessage(256);
 export const smallLengthField = errorMessage(128);
