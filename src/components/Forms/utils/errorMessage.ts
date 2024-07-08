98bb4c6f024913819bd5cf08605596be1c2d92b7
YuriKorepov <korepov@bravosoft.nnov.ru>
2024-07-08 18:44:40 +0300

    update error message text

--- src/components/Forms/utils/errorMessage.ts
+++ src/components/Forms/utils/errorMessage.ts
@@ -1,4 +1,4 @@
-const errorMessage = (size: number) => `Длина строки не может быть больше ${size} символов.`;
+const errorMessage = (size: number) => `Не более ${size} символов`;
 
 export const biggerLengthField = errorMessage(1024);
 export const mediumLengthField = errorMessage(512);
